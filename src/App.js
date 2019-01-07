9e675a176de6d6e315984c4837cb207382d2d30b
Ida Li <ida.li@nrm.se>
2019-01-07 10:28:22 +0100

    [UI] Add entry components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import configureStore from './store/configureStore'
+import browserHistory from './history'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import PrivateRoute from './router'
 
-export default App;
+import './styles/styles.css'
+
+import Admin from './views/Admin'
+import Public from './views/Public'
+
+const store = configureStore()
+const history = browserHistory()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <PrivateRoute component={Admin} path="/admin" />
+        <Route component={Public} path="/" />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
